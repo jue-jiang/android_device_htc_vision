0f5b4eba8cd02db33e9c10502f89b5ba61608efc
Jay Wu <cn.fyodor@gmail.com>
2012-11-20 23:48:57 +0800

    device: fix stupid typo
    Change-Id: I516952281339d856f6c8052ab77622485d6a5bda

--- device.mk
+++ device.mk
@@ -168,5 +168,5 @@ PRODUCT_PACKAGES += \
     Superuser
 
 # Publish that we support the live wallpaper feature
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
